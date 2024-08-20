f28d8f193c81d966a07bbdede23033b14ab9f594
sunghwan-joo <sunghwan.jooo@gmail.com>
2024-08-20 10:49:43 +0900

    demo web start

---
+++ src/main/java/com/example/demo/web/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "welcome";
+	}
+}
